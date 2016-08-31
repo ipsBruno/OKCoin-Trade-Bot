2ec47460b01c2b832d45fa9c621d804cf28c1642
ipsBruno <email@brunodasilva.com>
2016-08-31 15:25:25 -0300

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,12 @@
+echo Antes de tudo, voce precisa ter o NodeJS instalado!!
+echo Antes de tudo, voce precisa ter o NodeJS instalado!!
+echo Antes de tudo, voce precisa ter o NodeJS instalado!!
+echo Antes de tudo, voce precisa ter o NodeJS instalado!!
+echo Antes de tudo, voce precisa ter o NodeJS instalado!!
+echo Antes de tudo, voce precisa ter o NodeJS instalado!!
+echo Pressione qualquer tecla para tentar instalar
+echo Pressione qualquer tecla para tentar instalar
+echo Pressione qualquer tecla para tentar instalar
+pause
+start npm install okcoin
+start npm install dateformat
