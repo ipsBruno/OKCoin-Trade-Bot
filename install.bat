89fa7ea660182fe75ebd1cb90f6d162e3fa2ba64
Bruno da Silva <email@brunodasilva.com>
2016-09-03 17:00:04 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,3 @@
-echo Antes de tudo, voce precisa ter o NodeJS instalado!!
-echo Antes de tudo, voce precisa ter o NodeJS instalado!!
-echo Antes de tudo, voce precisa ter o NodeJS instalado!!
-echo Antes de tudo, voce precisa ter o NodeJS instalado!!
-echo Antes de tudo, voce precisa ter o NodeJS instalado!!
-echo Antes de tudo, voce precisa ter o NodeJS instalado!!
-echo Pressione qualquer tecla para tentar instalar
-echo Pressione qualquer tecla para tentar instalar
-echo Pressione qualquer tecla para tentar instalar
-pause
 start npm install okcoin
 start npm install dateformat
+pause
